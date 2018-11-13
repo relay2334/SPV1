83d92c4b38d4c1985fe94a3d2e71b7e5bac29fff
relay2334 <30300769+relay2334@users.noreply.github.com>
2018-11-12 23:20:30 -0500

    Create nmapFormat.sh

---
+++ lib/parsers/nmapFormat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Call Nmap Scan
+target=$1
+path="/tmp/nmapSheep1.txt"
+nmap -v --reason -oG $path -p- $target
+
+#Parse Output
+NMAP_FILE=$path
+egrep -v "^#|Status: Up" $NMAP_FILE | cut -d' ' -f2 -f4- | \
+sed -n -e 's/Ignored.*//p' | \
+awk -F, '{split($0,a," "); printf "Host: %-20s*%d\n" , a[1], NF}' \
+| sort -k 5 -g 
