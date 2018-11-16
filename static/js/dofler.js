1c912c8c4c1ce8377878fe644edcc67ea0d57b78
relay2334 <30300769+relay2334@users.noreply.github.com>
2018-11-16 12:06:52 -0500

    FIND THE ")"

--- static/js/dofler.js
+++ static/js/dofler.js
@@ -9,33 +9,33 @@ var flagged_images = [];
 function addImage(image) {
 	// Check to see if the image that was just sent to us is already being
 	// displayed.  If it isn't, then we will add it to the view.
-	var nsfw_ceiling = parseInt($('#nsfw-ceiling').val())
+	var nsfw_ceiling = parseInt($('#nsfw-ceiling').val());
 	if ($('img[src="/images/file/' + image.filename + '"]').length < 1
 		&& ((nsfw_ceiling && image.nsfw <  nsfw_ceiling) || !nsfw_ceiling)
 		&& flagged_images.indexOf(image.filename) < 0
 	){
 		if ($('#debug').is(':checked')){
-			console.log(image.filename + ' + with nsfw score ' + image.nsfw + ' drawn')
+			console.log(image.filename + ' + with nsfw score ' + image.nsfw + ' drawn');
 		}
 		$('#images').prepend('<img class="dofler-img" src="/images/file/' + image.filename + '" onclick="removeImage(\'' + image.filename + '\')">');
 	}else{
 		if ($('#debug').is(':checked')){
-			console.log(image.filename + ' with nsfw score ' + image.nsfw + ' NOT drawn')
+			console.log(image.filename + ' with nsfw score ' + image.nsfw + ' NOT drawn');
 		}
 	}
 
 	// As we only want to display the latest 200 images, we will want to
 	// remove the oldest images once this limit has been reached.
 	if ($('.dofler-img').length > 100){
-		$('.dofler-img:last').remove()
+		$('.dofler-img:last').remove();
 	}
 }
 
 
 function removeImage(filename) {
 	if ($('#removable-images').is(':checked')) {
-		$('img[src="/images/file/' + filename + '"]').remove()
-		flagged_images.push(filename)
+		$('img[src="/images/file/' + filename + '"]').remove();
+		flagged_images.push(filename);
 	}
 }
 
@@ -55,9 +55,9 @@ function displayHost(host, clip=true) {
 
 function renderHostList() {
 	if (hosts.length > 10) {
-		for (var i in hosts.slice(0,10)) {displayHost(hosts[i])}
+		for (var i in hosts.slice(0,10)) {displayHost(hosts[i]);}
 	} else {
-		for (var i in hosts) {displayHost(hosts[i])}
+		for (var i in hosts) {displayHost(hosts[i]);}
 	}
 }
 
@@ -77,7 +77,7 @@ function HostCycle() {
 		if (host_id > hosts.length - 1) {
 			host_id = host_id - hosts.length;
 		}
-		console.log(host_id)
+		console.log(host_id);
 
 		//TODO - Find way to display priority host
 		//Host with firstSeen and lastSeen values with 1 day of eachother
@@ -110,23 +110,23 @@ function addNetwork(network) {
 function protoRefresh(reporting = false) {
 	// Get the top 5 protocols from the API and then generate the flot graph
 	// based on the data returned.
-	var url = '/stats/protocols/5'
-	if (reporting) {url = '/stats/protoreport/10'}
+	var url = '/stats/protocols/5';
+	if (reporting) {url = '/stats/protoreport/10';}
 	$.getJSON(url, function(protos) {
 		$.plot('#proto-analysis', protos, 
 			{xaxis: { mode: "time", position: "right", timezone: "browser"},
 			legend: {position: "nw", backgroundColor: null, backgroundOpacity: 0}
 		});
-	})
+	});
 }
 
 
 function protoList() {
 	$.getJSON('/stats/protolist', function(protos) {
 		$.each(protos, function(key, proto) {
-			$('#proto-list').append('<li class="list-group-item"><span class="badge">' + proto.count + '</span>' + proto.name + '</li>')
-		})
-	})
+			$('#proto-list').append('<li class="list-group-item"><span class="badge">' + proto.count + '</span>' + proto.name + '</li>');
+		});
+	});
 }
 
 
@@ -145,9 +145,9 @@ function displayAccount(account, clip=true) {
 
 function renderAccountList() {
 	if (accounts.length > 10) {
-		for (var i in accounts.slice(0,10)) {displayAccount(accounts[i])}
+		for (var i in accounts.slice(0,10)) {displayAccount(accounts[i]);}
 	} else {
-		for (var i in accounts) {displayAccount(accounts[i])}
+		for (var i in accounts) {displayAccount(accounts[i]);}
 	}
 }
 
@@ -167,7 +167,7 @@ function accountCycle() {
 		if (account_id > accounts.length - 1) {
 			account_id = account_id - accounts.length;
 		}
-		console.log(account_id)
+		console.log(account_id);
 
 		// Now to remove the first entry and add the last entry into the
 		// user view.
@@ -197,9 +197,9 @@ function displayNetwork(network, clip=true) {
 
 function renderNetworkList() {
 	if (networks.length > 10) {
-		for (var i in networks.slice(0,10)) {displayNetwork(networks[i])}
+		for (var i in networks.slice(0,10)) {displayNetwork(networks[i]);}
 	} else {
-		for (var i in networks) {displayNetwork(networks[i])}
+		for (var i in networks) {displayNetwork(networks[i]);}
 	}
 }
 
@@ -219,7 +219,7 @@ function NetworkCycle() {
 		if (network_id > networks.length - 1) {
 			network_id = network_id - networks.length;
 		}
-		console.log(network_id)
+		console.log(network_id);
 
 		//TODO - Find way to display priority network
 		//Network with firstSeen and lastSeen values with 1 day of eachother
@@ -240,73 +240,67 @@ function report() {
 		$.getJSON('/images/common', function(images) {
 			$.each(images, function(key, image) {
 				$('#image-table').append('<tr><td>' + image.count + '</td><td><img class="dofler-img" src="/images/file/' + image.filename + '"></td></tr>');
-			})
+			});
 		});
 		$.getJSON('/accounts/list', function(accounts) {
 			$.each(accounts, function(key, account) {
 				addAccount(account);
 				displayAccount(account);
-			})
+			});
 		$.getJSON('/networks/list', function(networks) {
 			$.each(networks, function(key, network) {
 				addNetwork(network);
 				displayNetwork(network);
-			})
+			});
 		$.getJSON('/hosts/list', function(hosts) {
 			$.each(hosts, function(key, host) {
 				addHost(host);
 				displayHost(host);
-			})
+			});
 		});
 		protoRefresh(true);
-	})
-}
+	});
+});
 
 
 function display() {
 	socket.on('images', function(image) {
 		addImage(image);
-	})
+	});
 
 
 	socket.on('accounts', function(account) {
 		accounts.push(account);
-	})
+	});
 	
 	socket.on('networks', function(network) {
 		networks.push(network);
-	})
+	});
 	
 	socket.on('hosts', function(host) {
 		hosts.push(host);
-	})
+	});
 
 	socket.on('protocols', function(data){
 		protoRefresh();
-	})
+	});
+}
 
-
-
-
-	$(document).ready(function () {
+$(document).ready(function () {
 		$.getJSON('/images/list', function(images) {
 			$.each(images, function(key, image) {
 				addImage(image);
-			})
+			});
 		});
 		$.getJSON('/accounts/list', function(account_list) {
 			accounts = account_list;
 			renderAccountList();
 		});
-		$.getJSON('/hosts/list', function(host_list) {
-			hosts = host_list;
-			renderHostList();
-		});
-		$.getJSON('/networks/list', function(networks_list) { 
-                        networks = networks_list; 
-                        renderNetworkList(); 
-                });
+		$.getJSON('/vulns/hosts', function(data) {hostVulnList(data);});
+		$.getJSON('/vulns/vulns', function(data) {topVulnsList(data);});
 		protoRefresh();
 		setInterval(accountCycle, 1000);
-	})
-}
+	});
+
+
+  }
