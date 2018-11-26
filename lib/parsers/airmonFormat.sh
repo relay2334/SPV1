4ea79353ee363a4361a93cccc6aafcd26f697040
relay2334 <30300769+relay2334@users.noreply.github.com>
2018-11-26 12:06:50 -0500

    Update airmonFormat.sh

--- lib/parsers/airmonFormat.sh
+++ lib/parsers/airmonFormat.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 #Call Airodump
 interface=$1
-OUT_FILE=/tmp/airout.#FORMAT
-airodump-ng -i $interface -oC $OUT_FILE 
+OUT_FILE=/tmp/airout
+airodump-ng -w $OUT_FILE --output-format csv $interface
 
 https://www.cyberciti.biz/faq/unix-linux-bash-read-comma-separated-cvsfile/
 #Parse Output
-INPUT=data.cvs
+INPUT=/tmp/airout.csv
 OLDIFS=$IFS
 IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read flname dob ssn tel status
+while read bssid fs ls channel speed privacy cipher authentication pow beacons iv lip len essid key
 do
-	echo "Name : $flname"
-	echo "DOB : $dob"
-	echo "SSN : $ssn"
-	echo "Telephone : $tel"
-	echo "Status : $status"
+	printf "%s*%s*%s*%s*%s*" //ADD ARGS
 done < $INPUT
+	
 IFS=$OLDIFS
