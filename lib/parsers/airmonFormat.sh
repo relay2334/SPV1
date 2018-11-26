04a2b047f8b87b3faa6a72997d6d5d184f6e5cba
relay2334 <30300769+relay2334@users.noreply.github.com>
2018-11-26 13:10:26 -0500

    fix background

--- lib/parsers/airmonFormat.sh
+++ lib/parsers/airmonFormat.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #Call Airodump
 interface=$1
+delay=$2
 OUT_FILE=/tmp/airout
-airodump-ng -w $OUT_FILE --output-format csv $interface
+tmux new -d
+tmux send -Rt 0 airodump-ng SPACE -w SPACE $OUT_FILE SPACE --output-format SPACE csv SPACE $interface ENTER
+sleep $delay
+tmux send -Rt 0 CONTROL C
 
-https://www.cyberciti.biz/faq/unix-linux-bash-read-comma-separated-cvsfile/
 #Parse Output
 INPUT=/tmp/airout.csv
 OLDIFS=$IFS
@@ -12,7 +15,8 @@ IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read bssid fs ls channel speed privacy cipher authentication pow beacons iv lip len essid key
 do
-	printf "%s*%s*%s*%s*%s*" //ADD ARGS
+	printf "%s*%s*%s*%s*%s*" $essid $authentication $bssid $channel $pow
 done < $INPUT
-	
+printf "%s*%s*%s*%s*%s*" $essid $authentication $bssid $channel $pow
+
 IFS=$OLDIFS
